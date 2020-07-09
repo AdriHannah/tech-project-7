d065dc0bc8996aa46d88c36a526639677831fc5c
Adriana Jimenez <adriana.f.jimenez@gmail.com>
2020-07-09 13:17:55 -0400

    fix layout issue

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ let trafficChart = new Chart(trafficCanvas, {
 
 const dailyCanvas = document.getElementById("daily-chart");
 
+
 // data for daily traffic bar chart
 const dailyData = {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -87,7 +88,7 @@ let dailyChart = new Chart(dailyCanvas, {
 });
 
 
-//mobile chart//
+// mobile chart//
 const mobileCanvas = document.getElementById("mobile-chart");
 
 const mobileData = {
@@ -122,19 +123,19 @@ let mobileChart = new Chart(mobileCanvas, {
  
 // Messaging Section //
 
-const user = document.getElementById("userField");
-const message = document.getElementById("messageField");
-const send = document.getElementById("send");
+// const user = document.getElementById("userField");
+// const message = document.getElementById("messageField");
+// const send = document.getElementById("send");
 
-send.addEventListener('click', () => {
-    // ensure user and message fields are filled out
-    if (user.value === "" && message.value === "") {
-        alert("Please fill out user and message fields before sending");
-    } else if (user.value === "") {
-        alert("Please fill out user field before sending");
-    } else if (message.value === "") {
-        alert("Please fill out message field before sending");
-    } else {
-        alert(`Message successfully sent to: ${user.value}`);
-    }
-});
+// send.addEventListener('click', () => {
+//     // ensure user and message fields are filled out
+//     if (user.value === "" && message.value === "") {
+//         alert("Please fill out user and message fields before sending");
+//     } else if (user.value === "") {
+//         alert("Please fill out user field before sending");
+//     } else if (message.value === "") {
+//         alert("Please fill out message field before sending");
+//     } else {
+//         alert(`Message successfully sent to: ${user.value}`);
+//     }
+// });
